d05ab89a6567f1746a6c5ce6b6c9f0e76ff7baee
raaynaldo <raaynaldo@hotmail.com>
2020-11-19 12:44:58 -0500

    commit tree 5bf52a427ac9a81df3f36b335119ddc09939353d

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email) {
+  let data = {
+    name: name,
+    email: email,
+  };
+
+  let config = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  return fetch("http://localhost:3000/users", config)
+    .then((res) => res.json())
+    .then((json) => renderDOM(json.id))
+    .catch((err) => renderDOM(err.message));
+}
+
+function renderDOM(json) {
+  let body = document.querySelector("body");
+  console.log(body);
+  console.log(json);
+  body.innerHTML = json;
+}
